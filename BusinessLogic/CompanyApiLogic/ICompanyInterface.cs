68aa56af10e77f7fabfc15fe29cb237f61e7af5f
Lasse Poulsen <lp@intect.io>
2022-06-20 14:05:26 +0200

    Add ICompanyInterface

---
+++ BusinessLogic/CompanyApiLogic/ICompanyInterface.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.CompanyApiLogic
+{
+    public interface ICompanyInterface
+    {
+        List<Company> GetCompanies(); // get list of all companies
+        Company GetCompany(int id); // get one company with id
+        Company AddCompany (Company newCompany); // post Create new company
+        Company UpdateCompany(Company companyToUpdate); // Put, update an existing company
+        void DeleteCompany(Company companyToDelete); // Delete a company
+    } 
+}
